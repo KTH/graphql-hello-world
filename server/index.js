fd4a33a4ef871de6d33741964317257fa7c7ac3b
Carlos Saito <carlos@saito.style>
2019-10-10 13:40:05 +0200

    Remove deps

--- server/index.js
+++ server/index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
 const graphqlServer = require('./graphql')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
 app.get('/', (req, res) => {
-  res.cookie('courseId', req.query.canvasCourseId)
-  res.cookie('canvasToken', req.accessToken)
   res.send(
     `<!DOCTYPE html>
      <html lang="en">
